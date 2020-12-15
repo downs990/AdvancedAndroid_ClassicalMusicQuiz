8a6afab8bea7f2222ce5983406fad66c244caa61
downs990 <downscourtney17@gmail.com>
2020-12-15 16:57:27 -0500

    code formatting and spacing

--- app/src/main/java/com/example/android/classicalmusicquiz/MainActivity.java
+++ app/src/main/java/com/example/android/classicalmusicquiz/MainActivity.java
@@ -20,7 +20,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
